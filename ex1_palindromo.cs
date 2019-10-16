aaf5554180256373c10e6be00841c2da708eb545
Rodrigo Borges <rodrigo_craft@live.com>
2019-10-16 19:38:49 -0300

    Exercícios referente ao teste

---
+++ ex1_palindromo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+
+namespace RCATeste
+{
+    public class Ex1
+    {
+        private static bool EhPalindromo(string str)
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);  //registra provedor de encoding para acessar codificações
+            //Remove acentuação da palavra
+            byte[] bAcentoRemovido = Encoding.GetEncoding("windows-1251").GetBytes(str);
+            string acentoRemovido = Encoding.ASCII.GetString(bAcentoRemovido);
+            //Passando toda string para minúsculo para critério de comparação e deixar tudo igual
+            string lowerString = acentoRemovido.ToLower();
+            //Removendo espaços
+            string semEspacos = lowerString.Replace(" ", "");
+            //Removendo pontos
+            string finalString = semEspacos.Replace(".", "");
+            //Invertendo string para realizar comparação
+            string reverseString = new string(finalString.Reverse().ToArray());
+            Console.WriteLine(reverseString);
+            //Compara a string final (toda minúscula, sem acentos e sem espaços) com a mesma só que invertida
+            if (finalString == reverseString)
+                return true;
+
+            return false;
+        }
+
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Digite uma frase ou palavra: ");
+            string str = Console.ReadLine();
+            if (EhPalindromo(str))
+                Console.WriteLine(string.Format("A frase/palavra ({0}) é palíndromo!", str));
+            else
+                Console.WriteLine(string.Format("A frase/palavra ({0}) não é palíndromo!", str));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ex2.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RCATeste
+{
+    public class Ex2
+    {
+        private static List<string> palavras = new List<string>();
+
+        private static void ChecarPalavras()
+        {
+            int qtdDeA = 0;
+            int qtdPalavrasMinTresA = 0;
+            int qtdPalavrasComMaisQuatroLetras = 0;
+            //Percorre cada palavra em cada índice da lista de palavras
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("As palavras digitadas foram: ");
+            foreach(string s in palavras)
+            {
+                Console.WriteLine(s);
+                //Percorre cada caractere de cada palavra individualmente
+                for(int i = 0; i < s.Length; i++)
+                {
+                    if(s[i] == 'a')
+                    {
+                        ++qtdDeA;
+                        if(qtdDeA >= 3)
+                        {
+                            //A) incrementa cada vez que tiver no mínimo 3 a's na palavra
+                            ++qtdPalavrasMinTresA; 
+                            break; 
+                        }
+                    }
+                }
+                
+                //B) Caso a palavra em questão contenha mais de 4 letras
+                if (s.Length > 4)
+                    ++qtdPalavrasComMaisQuatroLetras;
+
+
+            }
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine(string.Format("A quantidade de palavras que possuem no mínimo 3 vezes a ocorrência da letra (a) são: {0}", qtdPalavrasMinTresA));
+            Console.WriteLine(string.Format("A quantidade de palavras que contém mais de 4 letras são: {0}", qtdPalavrasComMaisQuatroLetras));
+            Console.WriteLine("---------------------------------");
+            //C) Ordenar a lista de palavras por ordem alfabética
+            palavras.Sort();
+            Console.WriteLine("As palavras em ordem alfabética: ");
+            foreach(string s in palavras)
+                Console.WriteLine(s);
+        }
+
+        private static void Main()
+        {
+            int i = 0;
+            Console.WriteLine("Digite a quantidade de palavras a ser analisada: ");
+            string qtdPalavras = Console.ReadLine();
+            while(i < Convert.ToInt32(qtdPalavras))
+            {
+                string palavra = Console.ReadLine();
+                palavras.Add(palavra);
+                ++i;
+            }
+            ChecarPalavras();
+            Console.ReadKey();
+        }
+    }
+}
